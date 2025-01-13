9c1b482bd35abf5140dd66cb7aa1aeda3b0a2767
agnul <suCrabu@gmail.com>
2025-01-13 18:04:28 +0100

    Day 24 part 2, work in progress

--- 2024/python/day_24.py
+++ 2024/python/day_24.py
@@ -16,13 +16,41 @@ def ev(inputs, gates, g):
     i0, op, i1 = gates[g]
     if op == 'AND':
         return ev(inputs, gates, i0) and ev(inputs, gates, i1)
-    elif op == 'OR':
+    if op == 'OR':
         return ev(inputs, gates, i0) or ev(inputs, gates, i1)
-    elif op == 'XOR':
+    if op == 'XOR':
         a, b = ev(inputs, gates, i0), ev(inputs, gates, i1)
         return (a or b) and not a == b
-    else:
-        raise AssertionError(f'Invalid gate {op}')
+    raise AssertionError(f'Invalid gate {op}')
+
+def debug(circuit):
+    adders = [None] * 45
+    errs = []
+    for g in circuit:
+        i0, op, i1 = circuit[g]
+        if op == 'XOR':
+            if i0.startswith('x') or i0.startswith('y'):
+                i = int(i0[1:])
+                if not adders[i]: adders[i] = {}
+                adders[i]['inputs'] = (i0, i1)
+            elif g.startswith('z'):
+                i = int(g[1:])
+                if not adders[i]: adders[i] = {}
+                adders[i]['output'] = (i0, i1)
+            else:
+                print(f'{g} should not be the result of {i0} {op} {i1}')
+                errs.append(g)
+        elif op == 'AND':
+            if i0.startswith('x') or i0.startswith('y'):
+                i = int(i0[1:])
+                if not adders[i]: adders[i] = {}
+                adders[i]['carry'] = (i0, i1)
+        elif op == 'OR':
+            pass
+        else:
+            raise AssertionError(f'Invalid gate {op}.')
+    return adders
+
 
 def part_1(inputs, gates):
     z_bits = { b: ev(inputs, gates, b)
@@ -40,3 +68,5 @@ if __name__ == '__main__':
     inputs, circuit = parse_input(
         Path('../inputs/day_24.txt').read_text())
     print(part_1(inputs, circuit))
+
+    print(debug(circuit))
